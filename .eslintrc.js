d035e0cd0f96c98f8e7867340a63c4972a8ffa94
chenzhenfei <chenzhf@haier.com>
2021-08-19 09:16:11 +0800

    Update .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,125 +1,125 @@
 module.exports = {
-  'extends': ['plugin:import/errors'],
-  'plugins': ['import'],
-  'env': {
-    'es6': true,
-    'node': true
+  "extends": ["plugin:import/errors"],
+  "plugins": ["import"],
+  "env": {
+    "es6": true,
+    "node": true
   },
-  'parserOptions': {
-    'ecmaVersion': 6,
-    'sourceType': 'module',
-    'ecmaFeatures': {
-      'impliedStrict': true,
-      'objectLiteralDuplicateProperties': false
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "impliedStrict": true,
+      "objectLiteralDuplicateProperties": false
     }
   },
-  'rules': {
-    'array-bracket-spacing': ['error', 'never'],
+  "rules": {
+    "array-bracket-spacing": ["error", "never"],
 
-    'camelcase': ['error', {
-      'properties': 'never'
+    "camelcase": ["error", {
+      "properties": "never"
     }],
 
-    'comma-dangle': ['error', 'never'],
+    "comma-dangle": ["error", "never"],
 
-    'curly': ['error', 'all'],
+    "curly": ["error", "all"],
 
-    'eol-last': ['error'],
+    "eol-last": ["error"],
 
-    'indent': ['error', 2, {
-      'SwitchCase': 1
+    "indent": ["error", 2, {
+      "SwitchCase": 1
     }],
 
-    'keyword-spacing': ['error'],
+    "keyword-spacing": ["error"],
 
-    'max-len': ['error', {
-      'code': 180,
-      'ignoreComments': true,
-      'ignoreRegExpLiterals': true
+    "max-len": ["error", {
+      "code": 180,
+      "ignoreComments": true,
+      "ignoreRegExpLiterals": true
     }],
 
-    'no-else-return': ['error'],
+    "no-else-return": ["error"],
 
-    'no-mixed-spaces-and-tabs': ['error'],
+    "no-mixed-spaces-and-tabs": ["error"],
 
-    'no-multiple-empty-lines': ['error'],
+    "no-multiple-empty-lines": ["error"],
 
-    'no-spaced-func': ['error'],
+    "no-spaced-func": ["error"],
 
-    'no-trailing-spaces': ['error'],
+    "no-trailing-spaces": ["error"],
 
-    'no-undef': ['error'],
+    "no-undef": ["error"],
 
-    'no-unexpected-multiline': ['error'],
+    "no-unexpected-multiline": ["error"],
 
-    'no-unused-vars': ['error', {
-      'args': 'none',
-      'vars': 'all'
+    "no-unused-vars": ["error", {
+      "args": "none",
+      "vars": "all"
     }],
 
-    'quotes': ['error', 'single', {
-      'allowTemplateLiterals': true,
-      'avoidEscape': true
+    "quotes": ["error", "double", {
+      "allowTemplateLiterals": true,
+      "avoidEscape": true
     }],
 
-    'semi': ['error', 'never'],
+    "semi": ["error", "always"],
 
-    'space-before-blocks': ['error', 'always'],
+    "space-before-blocks": ["error", "always"],
 
-    'space-before-function-paren': ['error', 'never'],
+    "space-before-function-paren": ["error", "never"],
 
-    'space-in-parens': ['error', 'never'],
+    "space-in-parens": ["error", "never"],
 
-    'space-unary-ops': ['error', {
-      'nonwords': false,
-      'overrides': {}
+    "space-unary-ops": ["error", {
+      "nonwords": false,
+      "overrides": {}
     }],
 
-    // 'valid-jsdoc': ['error']
+    // "valid-jsdoc": ["error"]
 
     // ECMAScript 6 rules
 
-    'arrow-body-style': ['error', 'as-needed', {
-      'requireReturnForObjectLiteral': false
+    "arrow-body-style": ["error", "as-needed", {
+      "requireReturnForObjectLiteral": false
     }],
 
-    'arrow-parens': ['error', 'always'],
+    "arrow-parens": ["error", "always"],
 
-    'arrow-spacing': ['error', {
-      'after': true,
-      'before': true
+    "arrow-spacing": ["error", {
+      "after": true,
+      "before": true
     }],
 
-    'no-class-assign': ['error'],
+    "no-class-assign": ["error"],
 
-    'no-const-assign': ['error'],
+    "no-const-assign": ["error"],
 
-    'no-dupe-class-members': ['error'],
+    "no-dupe-class-members": ["error"],
 
-    'no-duplicate-imports': ['error'],
+    "no-duplicate-imports": ["error"],
 
-    'no-new-symbol': ['error'],
+    "no-new-symbol": ["error"],
 
-    'no-useless-rename': ['error'],
+    "no-useless-rename": ["error"],
 
-    'no-var': ['error'],
+    "no-var": ["error"],
 
-    'object-shorthand': ['error', 'always', {
-      'avoidQuotes': true,
-      'ignoreConstructors': false
+    "object-shorthand": ["error", "always", {
+      "avoidQuotes": true,
+      "ignoreConstructors": false
     }],
 
-    'prefer-arrow-callback': ['error', {
-      'allowNamedFunctions': false,
-      'allowUnboundThis': true
+    "prefer-arrow-callback": ["error", {
+      "allowNamedFunctions": false,
+      "allowUnboundThis": true
     }],
 
-    'prefer-const': ['error'],
+    "prefer-const": ["error"],
 
-    'prefer-rest-params': ['error'],
+    "prefer-rest-params": ["error"],
 
-    'prefer-template': ['error'],
+    "prefer-template": ["error"],
 
-    'template-curly-spacing': ['error', 'never']
+    "template-curly-spacing": ["error", "never"]
   }
-}
+};
--- test/reduceAdd.test.js
+++ test/reduceAdd.test.js
@@ -1,14 +1,15 @@
-import assert from 'assert'
-import reduceAdd from '../reduceAdd'
-
-describe('reduceAdd', () => {
+import assert from "assert";
+import reduceAdd from "../package/reduceAdd";
+;
+describe("reduceAdd", () => {
 
   it("should return the sum up", () => {
-    const array = [1, 2, 3, 4, 5]
-    assert.strictEqual(reduceAdd(array), 15)
+    const array = [1, 2, 3, 4, 5];
+    assert.strictEqual(reduceAdd(array), 15);
   });
-  it('should return the sum up', () => {
-    const array = [10, 20, 30, 40, 50]
-    assert.strictEqual(reduceAdd(array), 150)
+  it("should return the sum up", () => {
+    const array = [10, 20, 30, 40, 50];
+    assert.strictEqual(reduceAdd(array), 150);
   });
-})
+
+});
